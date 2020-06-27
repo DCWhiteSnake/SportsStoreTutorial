c6809f06ae446fd1b4a92ed7c069160f181354c2
David Okeke <davidokeke.c@gmail.com>
2020-06-27 20:52:14 +0100

    Add Pagination, add database.

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace SportsStore.Models
 {
-    public class EFRepository
+    public class EFProductRepository:IProductRepository
     {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext cxt)
+        {
+            context = cxt;
+        }
+
+        public IQueryable<Product> Products => context.Products;
     }
 }
