71b938d5c628f93bfb488937196da3665bd14a6e
David Okeke <davidokeke.c@gmail.com>
2020-06-30 00:58:22 +0100

    Add View Models folder, define PadeInfo View Model to pass information to the PageLink taghelper.

---
+++ SportsStore/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SportsStore.Models.ViewModels
+{
+    // This view model is used to pass information
+    // about the number of pages to the view model.
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        // This method uses the toal number of items to
+        // get the total number of pages by some simple math.
+        public int TotalPages =>
+            (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
