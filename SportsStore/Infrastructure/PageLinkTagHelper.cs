af5fb5a3ecd55da46c95a39ce65d027a259252ac
David Okeke <davidokeke.c@gmail.com>
2020-07-13 01:23:27 +0100

    Minor House keeping

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using SportsStore.Models.ViewModels;
+using System.Collections.Generic;
 
 // Infrastruce folder is used to house other things not accomodated
 // by the models folder.
@@ -27,6 +28,11 @@ namespace SportsStore.Infrastructure
 
         public string PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+            = new Dictionary<string, object>();
+
+
         public string PageClass { get; set; }
 
         public bool PageClassesEnabled { get; set; } = false;
@@ -39,7 +45,8 @@ namespace SportsStore.Infrastructure
 
         [ViewContext]
         [HtmlAttributeNotBound]
-        public ViewContext ViewContext { get; set; }
+        public ViewContext ViewContext { get; set; } // Vie
+        
         public override void Process(TagHelperContext context,
                 TagHelperOutput output)
         {
@@ -51,8 +58,9 @@ namespace SportsStore.Infrastructure
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["productPage"] = i;
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
-                  new { productPage = i });
+                  PageUrlValues);
                 if (PageClassesEnabled)
                 {
                     tag.AddCssClass(PageClass);
